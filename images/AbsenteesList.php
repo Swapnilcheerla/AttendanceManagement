16a8a4e9f4a0b60ab96bf68db8045634d47ea6a2
Swapnil Cheerla <cheerla.swapnilreddy@gmail.com>
2020-03-09 23:51:00 +0530

    updated files

--- images/AbsenteesList.php
+++ images/AbsenteesList.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if( !isset($_SESSION["hod_email"]) ){
+  header("location:HODLogin.php");
+  exit();
+}
+?>
+
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
@@ -14,7 +22,7 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 </head>
-<body>
+<body onload="automaticDate()">
     <div class="container-fluid " id="jumbotroncon">
              <div class="jumbotron container-fluid text-center" id="jum">
                 <div id="jumleft">
@@ -45,13 +53,10 @@
             <div id="main-menu" class="main-menu collapse navbar-collapse">
                 <ul class="nav navbar-nav">
                     <li class="active">
-                        <a href="Principallogin.php">Principal Login </a>
+                        <a href="SelectSubject.php">Take Attendence</a>
                     </li>
                     <li class="active">
-                        <a href="HODLogin.php">HOD Login</a>
-                    </li>
-                    <li class="active">
-                        <a href="StaffLogin.php">Staff Login</a>
+                        <a href="Take Attendence.php">Change Subject</a>
                     </li>
                     
 
@@ -73,11 +78,19 @@
             <div class="header-menu">
                 <div class="col-sm-7">
                     <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
+                    
                 </div>
 
-                <div class="col-sm-5">
+               <div class="col-sm-5">
                     <div class="user-area dropdown float-right">
-                        
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="     false">
+                            <img class="user-avatar rounded-circle" src="images/profile1.png" alt="User Avatar">
+                        </a>
+
+                        <div class="user-menu dropdown-menu">
+                            <a class="nav-link" href="#"><i class="fa fa-user"></i> My Profile</a>
+                            <a class="nav-link" href="Logout.php"><i class="fa fa-power-off"></i> Logout</a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -85,9 +98,34 @@
         </header><!-- /header -->
         <!-- Header-->
 
-        
-        
-
+  
+<?php
+if($_GET){
+    $conn = new mysqli('localhost','root','',$_SESSION["hod_department"]);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $year=$_GET["year"];
+    $section=$_GET["section"];
+    $date=$_SESSION["dateofabsentees"];
+    $hour="hour".$_GET["hour"];
+    $sql="  SELECT `student_id`,`student_name` from `year".$year."_daily_attendance` where student_section='$section' and `$hour`='0' and `date`='$date'";
+    $result=$conn->query($sql);
+    echo "<div class='container'><h4>Year: $year Section:$section No.of.Absentees: ".mysqli_num_rows ( $result )."</h4>";
+    echo "<table class='table'><thead class='thead-dark'><tr><th>Student ID</th><th>Student Name</th><th>Student Number</th></thead><tbody>";
+    echo "<a href='HourInput.php'>Choose another Year</a><br>";
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>".$row["student_id"]."</td><td>".$row["student_name"]."</td><td>9494103952</td>";
+            echo "<tr>";
+        }
+    } else {
+        echo "<h4>O Absentees</h4>";
+    }
+    echo "</tbody><table>";
+    $conn->close();
+}?>
 
     </div>
 </div><!-- .content -->
@@ -104,5 +142,3 @@
 
 
 </body>
-
-</html>
---
+++ weeklybranchyear.php
@@ -0,0 +1,166 @@
+<?php
+session_start();
+if( !isset($_SESSION["p_email"]) ){
+  header("location:Principallogin.php");
+  exit();
+}
+
+?>
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MREC </title>
+    <link rel="stylesheet" type="text/css" href="modal.css">
+    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="vendors/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendors/themify-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="vendors/selectFX/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+</head>
+<body onload="automaticDate()">
+    <div class="container-fluid " id="jumbotroncon">
+             <div class="jumbotron container-fluid text-center" id="jum">
+                <div id="jumleft">
+                    <img src="logo" alt="logo">
+                </div>
+                <div >
+                    <h2 class="display"><strong>MALLAREDDY ENGINNERING COLLEGE (AUTONOMOUS)</strong></h2>
+                  <h6>( An Autonomous institution approved by UGC and Affiliated to JNTUH Hyderabad,</h6>
+                  <h6>        Accredited by NAAC with 'A' Grade,Accredited by NBA,    </h6>
+                  <h6> Maisammaguda, dhulapally,(Post, via Kompally),Secunderabad-500100 Ph:040-64634234)</h6>
+                </div>
+                  
+             </div>
+        </div>
+    
+
+    <div>
+        <aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default">
+
+            <div class="navbar-header">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="./">MREC</a>
+            </div>
+
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Daily</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><a href="principalsidenav.html">All Branches</a></li>
+                            <li><a href="principalsidenav.html">Branch-year-wise</a></li>
+                            <li><a href="#" id="modal-btn">Branch-section-wise</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Weekly</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><a href="principalsidenav.html">All Branches</a></li>
+                            <li><a href="principalsidenav.html">Branch-hour-wise</a></li>
+                            <li><a href="principalsidenav.html">Branch-year-wise</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Monthly</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><a href="branch_wise_monthly.php">All Branches</a></li>
+                            <li><a href="principalsidenav.html">Branch-hour-wise</a></li>
+                            <li><a href="principalsidenav.html">Branch-year-wise</a></li>
+                        </ul>
+                    </li>
+
+                    
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+    </aside><!-- /#left-panel -->
+
+    <!-- Left Panel -->
+
+    <!-- Right Panel -->
+
+    <div id="right-panel" class="right-panel">
+
+        <!-- Header-->
+        <header id="header" class="header">
+
+            <div class="header-menu">
+
+                <div class="col-sm-7">
+                    <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
+                    <h4><strong><?php echo strtoupper($_SESSION["p_name"]);?></strong> </h4>
+                     </h4>
+                </div>
+
+                <div class="col-sm-5">
+                    <div class="user-area dropdown float-right">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="     false">
+                            <img class="user-avatar rounded-circle" src="images/profile1.png" alt="User Avatar">
+                        </a>
+
+                        <div class="user-menu dropdown-menu">
+                            <a class="nav-link" href="#"><i class="fa fa-user"></i> My Profile</a>
+                            <a class="nav-link" href="Logout.php"><i class="fa fa-power-off"></i> Logout</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </header><!-- /header -->
+        <!-- Header-->
+
+
+<div class="container">
+    <form action="branch_year_wise_weekly.php" method="post">
+     <div class="form-group">
+      <label for="department">Select Department: </label>
+      <select class="form-control" id="department" name="department">
+      <option value='sd' selected>Select Department</option>
+        <option value='CSE'>CSE</option>
+        <option value="ECE">ECE</option>
+        <option value='EEE'>EEE</option>
+        <option value='IT'>IT</option>
+        <option value='MECH'>MECH</option>
+        <option value='CIVIL'>CIVIL</option>
+        <option value='MBA'>MBA</option>
+      </select> 
+    </div>
+       
+    
+    <div class="form-group">
+      <label for="number_of_hours">Select Hour:</label>
+      <input type="number" class="form-control" name="hour" min='1' required placeholder="Select Hour">
+    </div>
+    
+          <input type="submit" value="GET DATA">
+    </form>
+</div>
+
+</div>
+</div><!-- .content -->
+    </div><!-- /#right-panel -->
+    </div>
+    <!-- Left Panel -->
+    <!-- Right Panel -->
+
+
+    <script src="vendors/jquery/dist/jquery.min.js"></script>
+    <script src="vendors/popper.js/dist/umd/popper.min.js"></script>
+    <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="modal.js"></script>
+
+
+</body>
+
+</html>
+
